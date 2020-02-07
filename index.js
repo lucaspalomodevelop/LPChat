a6dfbea373ef6cfec9d35fff5f70a9fdfddeb8ce
lucaspalomodevelop <lucas.palomo@t-online.de>
2020-02-07 17:44:06 +0100

    implemented sql require and express-session // format all

--- index.js
+++ index.js
@@ -3,19 +3,75 @@
 let InfaStruc = require("./modules/PalomoLibs/Palomo.Infrastructure");
 // app.js
 let express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var path = require('path');
 let app = express();
 let server = require('http').createServer(app);
 let io = require('socket.io')(server);
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodelogin'
+});
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-app.use(express.static("views"));
 app.use(InfaStruc.functions.expresslogger)
+app.use(express.static("views"));
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get('/', function(req, res,next) {
-    res.render("index",{InfaStruc:InfaStruc});
+app.get('/', function (req, res, next) {
+    res.redirect("/login");
 });
 
-server.listen(InfaStruc.properties.server.port,() => {
+
+app.get('/login', function (req, res, next) {
+    res.render("index", { InfaStruc: InfaStruc });
+});
+
+app.get('/home', function (request, response) {
+    if (request.session.loggedin) {
+        response.send('Welcome back, ' + request.session.username + '!');
+    } else {
+        response.send('Please login to view this page!');
+    }
+    response.end();
+});
+
+app.post('/auth', function (request, response) {
+    var username = request.body.username;
+    var password = request.body.password;
+    if (username && password) {
+        connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function (error, results, fields) {
+            if (results.length > 0) {
+                request.session.loggedin = true;
+                request.session.username = username;
+                response.redirect('/home');
+            } else {
+                response.send('Incorrect Username and/or Password!');
+            }
+            response.end();
+        });
+    } else {
+        response.send('Please enter Username and Password!');
+        response.end();
+    }
+});
+
+
+
+server.listen(InfaStruc.properties.server.port, () => {
     console.log("Server Listening on port " + InfaStruc.properties.server.port);
+    console.log("http://127.0.0.1:" + InfaStruc.properties.server.port)
 });
--- modules/PalomoLibs/Palomo.Infrastructure.js
+++ modules/PalomoLibs/Palomo.Infrastructure.js
@@ -5,27 +5,27 @@ let properties_path = __dirname + "/../../ServerProperties.json"
 
 let struc = {}
 
-struc.vars = 
+struc.vars =
 {
 
 }
 
 struc.functions =
 {
-    "timestemp" : () => { return moment().format("YYYY_MM_DD : HH:mm:ss")},
-    "expresslogger" : (req, res,next) => {
-    
-        let string = `${struc.functions.timestemp()} (${req.method})`
-    console.log(string);
-    next();
+    "timestemp": () => { return moment().format("YYYY_MM_DD : HH:mm:ss") },
+    "expresslogger": (req, res, next) => {
+        var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        let string = `${struc.functions.timestemp()} -> ${ip} <- (${req.method}) ${req.originalUrl}`
+        console.log(string);
+        next();
     }
 }
 
 
-if(fs.existsSync(properties_path))
-struc.properties = require(properties_path);
+if (fs.existsSync(properties_path))
+    struc.properties = require(properties_path);
 else
-console.dir(fs.existsSync(properties_path) + " " + properties_path);
+    console.dir(fs.existsSync(properties_path) + " " + properties_path);
 
 
 
